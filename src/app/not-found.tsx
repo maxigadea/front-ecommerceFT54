5375e17e0e95153e08dbaa7517fda4c1078b77bb
maxigadea <maxi.gadea@bitnovo.com>
2024-10-21 12:18:08 -0300

    fix in not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -23,7 +23,7 @@ export default function NotFound() {
         </svg>
       </div>
       <p className="mt-4 text-gray-600">
-        Let's get you back{" "}
+        Lets get you back{" "}
         <Link href="/" className="text-blue-500">
           home
         </Link>
